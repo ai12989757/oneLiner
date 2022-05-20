bf572bb87849fc65b7b0b6f91fb5f7c5cd1b3221
yibai <159689757@qq.com>
2022-05-20 12:16:07 +0800

    main

---
+++ MainWindow.py
@@ -0,0 +1,154 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from PySide2.QtCore import *
+from PySide2.QtGui import *
+from PySide2.QtWidgets import *
+
+from shiboken2 import wrapInstance
+import maya.OpenMayaUI as omui
+from pymel.core import *
+
+from main import Ui_Form
+from oneLiner import *
+
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object
+    """
+    main_window_ptr = omui.MQtUtil.mainWindow()
+    try:
+        return wrapInstance(long(main_window_ptr), QWidget)
+    except:
+        return wrapInstance(int(main_window_ptr), QWidget)
+    # python3.x版本已删除long() 函数, 用int替代
+    
+class oneLinerUI(Ui_Form, QWidget):
+    def __init__(self, parent=maya_main_window()):
+        super(oneLinerUI, self).__init__(parent)
+        self.setupUi(self)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)                           # 设置窗口背景透明
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Window)                  # 设置窗口无边框
+        self.setGeometry(QCursor.pos().x()-40, QCursor.pos().y()-15, 320,20)     # 根据光标位置设置窗口位置
+ 
+        # ListView Set
+        self.lineEdit.returnPressed.connect(self.runCommand)                     # 回车键触发事件, 执行命令
+        self.lineEdit.setContextMenuPolicy(Qt.CustomContextMenu)                 # 鼠标右击触发事件, 显示提示文本
+        self.lineEdit.customContextMenuRequested.connect(self.showHelpTips)      # 链接弹出菜单
+        self.lineEdit.setFocus()                                                 # 激活lineEdit输入框
+
+        sel = [l.name() for l in ls(sl=True)]
+        if len(sel) == 1:
+            self.resize(304, 44)
+            self.lineEdit.setPlaceholderText("输入以重命名, 右击查看帮助")
+        elif len(sel) == 0:
+            self.resize(304, 24)
+            self.lineEdit.setPlaceholderText("请选择物体, 或键入f:/fs:/fe: + 字符 以选择相关对象")
+            # 不显示listView
+            self.listView.setVisible(False)
+        else:
+            self.resize(330420, len(sel)*(18-(0.03*len(sel)))+24)                # 根据选中对象的数量动态调整弹窗的大小
+            self.lineEdit.setPlaceholderText("输入以重命名, 右击查看帮助")
+        self.lineEdit.textChanged.connect(self.changeList)                       # 输入框文本改变时，预览修改后的名称
+
+        # ListView Set
+        self.listView.setModel(QStringListModel(sel))                            # 将选中物体的名称添加到listView中
+        self.listView.setEditTriggers(QAbstractItemView.NoEditTriggers)          # 列表不可操作
+        self.listView.setSelectionMode(QAbstractItemView.NoSelection)            # 不可选中
+        self.listView.setFocusPolicy(Qt.NoFocus)                                 # 不可活动
+        self.listView.setStyleSheet("QListView::item:hover { background: #3F7EA8;} QListView{ background: transparent;}") # 鼠标悬停背景颜色颜色
+        self.listView.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)          # 关闭垂直滚动条
+        
+    def paintEvent(self, event):
+        # 圆角
+        painter = QPainter(self)
+        painter.setRenderHint(painter.Antialiasing)                              # 抗锯齿
+        painter.setBrush(QBrush(QColor(0,0,0,100)))                              # 设置画出边框的颜色,RGBA 255
+        painter.setPen(QPen(QColor(0,0,0,100)))                                  # 设置画出边框的颜色,RGBA 255
+        painter.drawRoundedRect( 0, 0, self.width(), self.height(), 5, 5);       # 圆角设置
+
+    def event(self, eve):
+        if eve.type() == QEvent.WindowDeactivate:
+            self.close()
+        return QWidget.event(self, eve)
+
+    def changeList(self):
+        text = self.lineEdit.text()
+        if text:
+            self.listView.setModel(QStringListModel(newNameView(text)))
+            if len(newNameView(text)) == 1:
+                self.resize(304, 44)
+            else:
+                if ls(sl=True):
+                    self.resize(304, len(newNameView(text))*(18-(0.03*len(newNameView(text))))+24) 
+                else:
+                    self.resize(304, 24)
+        else:
+            self.listView.setModel(QStringListModel([l.name() for l in ls(sl=True)]))
+
+    def runCommand(self):
+        text = self.lineEdit.text()
+        if text:
+            self.lineEdit.clear()
+            oneLiner(text)
+            self.listView.setModel(QStringListModel([l.name() for l in ls(sl=True)]))
+            if len(ls(sl=True,fl=True)) == 1:
+                self.resize(304, 44)
+                self.lineEdit.setPlaceholderText("输入以重命名, 右击查看帮助") 
+            elif len(ls(sl=True,fl=True)) == 0:
+                self.resize(304, 24)
+                self.lineEdit.setPlaceholderText("请选择物体, 或键入f:/fs:/fe: + 字符 以选择相关对象") 
+            else:
+                self.listView.setVisible(True)
+                self.resize(304, len(ls(sl=True,fl=True))*(18-(0.03*len(ls(sl=True,fl=True))))+24)                    
+                   
+    def showHelpTips(self):
+        menu = QMenu()
+        
+        tip1 = menu.addAction('符号表示字符:')
+        tip1.setFont(QFont("Microsoft YaHei", 10, QFont.Bold))
+        tip1.setIcon(QIcon("D:/MEL/OneTools/tools/oneLiner/icon/help.png"))
+        # menu.addAction('执行命令').triggered.connect(self.runCommand) # 执行命令
+        menu.addAction('! = 原名称(可以是名称的一部分)')
+        menu.addAction('# = 代表数字, 可以多个##代表多个数字')
+        menu.addAction('@ = 按选择的顺序用大写英文字母排序')
+        menu.addSeparator()
+        
+        tip1 = menu.addAction('查找和替换:')
+        tip1.setFont(QFont("Microsoft YaHei", 10, QFont.Bold))
+        tip1.setIcon(QIcon("D:/MEL/OneTools/tools/oneLiner/icon/search.png"))
+        menu.addAction('" 原名称 " > " 新名称 " (可以是名称的一部分)')
+        menu.addSeparator()
+
+        tip1 = menu.addAction('删除字符:')
+        tip1.setFont(QFont("Microsoft YaHei", 10, QFont.Bold))
+        tip1.setIcon(QIcon("D:/MEL/OneTools/tools/oneLiner/icon/clear.png"))
+        menu.addAction('" + 数字 " = 从开端删除几个字符')
+        menu.addAction('" - 数字 " = 从末尾删除几个字符')
+        menu.addAction('" -- 数字 " = 删到只剩几个字符(保留前面几个字符)')
+        menu.addSeparator()
+
+        tip1 = menu.addAction('选择对象/层级:')
+        tip1.setFont(QFont("Microsoft YaHei", 10, QFont.Bold))
+        tip1.setIcon(QIcon("D:/MEL/OneTools/tools/oneLiner/icon/selection.png"))
+        menu.addAction('"/s" 默认设置, 基于当前选择对象进行重命名')
+        menu.addAction('"/h" 选择对象的所有层级进行重命名')
+        menu.addSeparator()
+
+        tip1 = menu.addAction('附加功能:')
+        tip1.setFont(QFont("Microsoft YaHei", 10, QFont.Bold))
+        tip1.setIcon(QIcon("D:/MEL/OneTools/tools/oneLiner/icon/ps.png"))
+        menu.addAction('"f:" 用法类似 ls "*name*"')
+        menu.addAction('"fs:" 用法类似 ls "name*"')
+        menu.addAction('"fe:" 用法类似 ls "*name"')
+        menu.exec_(QCursor.pos())
+
+if __name__ == "__main__":
+    try:
+        olUI.close()
+        olUI.deleteLater()
+    except:
+        pass
+
+    olUI = oneLinerUI()
+    olUI.show()
---
+++ oneLiner.py
@@ -0,0 +1,258 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#---------written by:----------------------
+#-------Fauzan Syabana---------------------
+#------zansyabana@gmail.com----------------
+#Licensed under MIT License
+#------------------------------------------
+#----------change by:----------------------
+#------------yibai-------------------------
+#--------159689757@qq.com------------------
+# Change Log: 
+# - support python 3.x
+# - create new UI
+#------------------------------------------
+
+from pymel.core import *
+
+#selector
+def selector(lookName):
+    nameSplit = lookName.split(':')
+    method = nameSplit[0]
+    nameSel = nameSplit[1]
+    if method == 'f':
+        sel = ls("*{}*".format(nameSel), r=True)
+    elif method == 'fe':
+        sel = ls("*{}".format(nameSel), r=True)
+    elif method == 'fs':
+        sel = ls("{}*".format(nameSel), r=True)
+    select(sel)
+
+def oneLiner(nName, method='s'):
+
+    # get selection method
+    if nName.find(':') != -1:
+        selector(nName)
+    elif nName.find('/s') != -1:
+        method = 's'
+        nName = nName.replace('/s', '')
+    elif nName.find('/h') != -1:
+        method = 'h'
+        nName = nName.replace('/h', '')
+    elif nName.find('/a') != -1:
+        if nName.find('>') != -1:
+            method = 'a'
+            print(method)
+        nName = nName.replace('/a', '')
+
+    if method == 's':
+        slt = selected()
+    elif method == 'h':
+        sltH = []
+        slt = selected()
+        for i in slt:
+            sltH.append(i)
+            for child in reversed(i.listRelatives(ad=True, type='transform')):
+                sltH.append(child)
+        print(sltH)
+        slt = sltH
+    elif method == 'a':
+        slt = ls()
+
+    # find numbering replacement
+    def numReplace(numName, idx, start=1):
+        global padding
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if numName.find('//') != -1:
+            start = int(numName[numName.find('//') + 2:len(numName)])
+            numName = numName.replace(numName[numName.find('//'):len(numName)], '')
+            print(start)
+        number = idx + start
+        if numName.find('#') != -1:
+            padding = numName.count('#')
+            hastag = "{0:#>{1}}".format("#", padding)  # get how many '#' is in the new name
+            num = "{0:0>{1}d}".format(number, padding)  # get number
+            numName = numName.replace(hastag, num)
+        # alphabetical numbering
+        if numName.find('@') != -1:
+            padding = numName.count('@')
+            aSym = "{0:@>{1}}".format("@", padding) # get how many '@' is in the new name
+            alphaNum = "{}".format(alpha[idx])  # get alphabetical number
+            numName = numName.replace(aSym, alphaNum)
+
+        return numName
+    if nName.find(':') == -1:
+        for i in slt:  # for every object in selection list
+            # check if there is '>' that represents the replacement method
+            if i.name().find('|') !=-1:
+                curName = i.name().split('|')[-1]
+            else:
+                curName = i.name()
+            if nName.find('>') != -1:
+                wordSplit = nName.split('>')
+                oldWord = wordSplit[0]
+                newWord = numReplace(wordSplit[1],slt.index(i))
+                try:
+                    rename(i,i.replace(oldWord,newWord))
+                except:
+                    print("{} is not renamed".format(i))
+
+            # check if the first character is '-' or '+', remove character method
+            elif nName[0] == '-':
+                charToRemove = int(nName[1:len(nName)])
+                try:
+                    rename(i, i[0:-charToRemove])
+                except:
+                    print("{} is not renamed".format(i))
+
+            elif nName[0] == '+':
+                charToRemove = int(nName[1:len(nName)])
+                rename(i, i[charToRemove:len(curName)])
+
+            else:
+                newName = numReplace(nName, slt.index(i))
+                # get current Name if '!' mentioned
+                print(i)
+                if newName.find('!') != -1:
+                    newName = newName.replace('!', curName)
+                    print(newName)
+                try:
+                    rename(i, newName)
+                except:
+                    print("{} is not renamed".format(i))
+
+def newNameView(nName, method='s'):
+    changeName = []
+    # get selection method
+    if nName.find(':') != -1:
+        pass
+        #selector(nName)
+    elif nName.find('/s') != -1:
+        method = 's'
+        nName = nName.replace('/s', '')
+    elif nName.find('/h') != -1:
+        method = 'h'
+        nName = nName.replace('/h', '')
+    elif nName.find('/a') != -1:
+        if nName.find('>') != -1:
+            method = 'a'
+            #print(method)
+        nName = nName.replace('/a', '')
+
+    if method == 's':
+        slt = selected()
+    elif method == 'h':
+        sltH = []
+        slt = selected()
+        for i in slt:
+            sltH.append(i)
+            for child in reversed(i.listRelatives(ad=True, type='transform')):
+                sltH.append(child)
+        #print(sltH)
+        slt = sltH
+    elif method == 'a':
+        slt = ls()
+
+    # find numbering replacement
+    def numReplace(numName, idx, start=1):
+        global padding
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        if numName.find('//') != -1:
+            start = int(numName[numName.find('//') + 2:len(numName)])
+            numName = numName.replace(numName[numName.find('//'):len(numName)], '')
+            #print(start)
+        number = idx + start
+        if numName.find('#') != -1:
+            padding = numName.count('#')
+            hastag = "{0:#>{1}}".format("#", padding)  # get how many '#' is in the new name
+            num = "{0:0>{1}d}".format(number, padding)  # get number
+            numName = numName.replace(hastag, num)
+        # alphabetical numbering
+        if numName.find('@') != -1:
+            padding = numName.count('@')
+            aSym = "{0:@>{1}}".format("@", padding) # get how many '@' is in the new name
+            alphaNum = "{}".format(alpha[idx])  # get alphabetical number
+            numName = numName.replace(aSym, alphaNum)
+
+        return numName
+    if nName.find(':') == -1:
+        for i in slt:  # for every object in selection list
+            # check if there is '>' that represents the replacement method
+            if i.name().find('|') !=-1:
+                curName = i.name().split('|')[-1]
+            else:
+                curName = i.name()
+                
+            if nName.find('>') != -1:
+                wordSplit = nName.split('>')
+                oldWord = wordSplit[0]
+                newWord = numReplace(wordSplit[1],slt.index(i))
+                newName = i.replace(oldWord,newWord)
+                changeName.append(newName)
+            # check if the first character is '-' or '+', remove character method
+            elif nName[0] == '-':
+                charToRemove = int(nName[1:len(nName)])
+                newName = i[0:-charToRemove]
+                changeName.append(newName)
+            elif nName[0] == '+':
+                charToRemove = int(nName[1:len(nName)])
+                newName = i[charToRemove:len(curName)]
+                changeName.append(newName)
+            else:
+                newName = numReplace(nName, slt.index(i))
+                # get current Name if '!' mentioned
+                #print(i)
+                if newName.find('!') != -1:
+                    newName = newName.replace('!', curName)
+                changeName.append(newName)
+        
+    return changeName
+"""
+### UI
+
+class oneLinerWindow(object):
+
+    windowName = "OneLiner"
+
+    def show(self):
+
+        if window(self.windowName, q=True, exists=True):
+            deleteUI(self.windowName)
+            windowPref(self.windowName, remove=True)
+        window(self.windowName, s=True, w=300, h=10, rtf=False, tb=True)
+
+        self.buildUI()
+
+        showWindow()
+
+    def buildUI(self):
+        toolTip = 'Character replacement symbols:' \
+                  '\n! = old name' \
+                  '\n# = numbering based on selection, add more # for more digits' \
+                  '\n@ = alphabetical numbering based on selection' \
+                  '\n\nFind and replace method:' \
+                  '\n"oldName">"newName" (without quotes)' \
+                  '\n\nRemove first or last character(s):' \
+                  '\n-(amount of characters to remove) = removes specific amounts of characters from last character'\
+                  '\n+(amount of characters to remove) = removes specific amounts of characters from first character' \
+                  '\n\nAdd these symbols at the end to change the options:' \
+                  '\n//(number) = define the start number of numbering from #' \
+                  '\n/s = selected only (this is default, you dont have to type this)' \
+                  '\n/h = add items from all hierarchy descendants of selected items' \
+                  '\n\nAdditional tool:'\
+                  '\nadd f: at the start of the text to find objects within desired characters'\
+                  '\nadd fe: at the start of the text to find objects that ends with the desired characters'\
+                  '\nadd f: at the start of the text to find objects the starts with the desired scharacters'
+
+        column = columnLayout(cal='center', adj=True)
+        separator(h=1, style='none')
+        self.rnmInput = textField(ec=self.runFunc, aie=True, w=200, ann=toolTip)
+        separator(h=1, style='none')
+
+    def runFunc(self,*args):
+        self.rnmQ = textField(self.rnmInput, text=True,q=True)
+        oneLiner(self.rnmQ)
+        #window(self.windowName, e=True, vis=False) # 成功执行后隐藏窗口
+
+oneLinerWindow().show()
+"""
